2fa5dfd69f98bc7bab5360f1c18b4c729b53014d
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-10-07 20:11:50 -0500

    perf: unsubscribe when destroying `RandomPageComponent`

--- src/app/quotes/pages/random-quote-page/random-quote-page.component.ts
+++ src/app/quotes/pages/random-quote-page/random-quote-page.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { QuotesService } from '../../services/quotes.service';
 import { ColorThemesService } from '../../services/color-themes.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-random-quote-page',
   templateUrl: './random-quote-page.component.html',
   styleUrls: ['./random-quote-page.component.scss'],
 })
-export class RandomQuotePageComponent implements OnInit {
+export class RandomQuotePageComponent implements OnInit, OnDestroy {
   colorTheme!: string;
+  colorThemeSubscription!: Subscription;
+
   constructor(
     private quotesService: QuotesService,
     private colorThemesService: ColorThemesService
@@ -19,8 +22,12 @@ export class RandomQuotePageComponent implements OnInit {
     this.quotesService
       .loadQuotes()
       .subscribe((_) => this.quotesService.emitNextRandomQuote());
-    this.colorThemesService
+    this.colorThemeSubscription = this.colorThemesService
       .getRandomRGBColor()
       .subscribe((color) => (this.colorTheme = color));
+  }
+
+  ngOnDestroy(): void {
+    this.colorThemeSubscription.unsubscribe();
   }
 }
